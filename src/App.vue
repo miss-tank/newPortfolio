5b4d3fe21d8b220e7c16857c2b912d2323f45b73
Ankita Tank <ANKITA.TANK@morningstar.com>
2020-04-23 13:57:13 -0500

    Create a new vue app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="new Tank portfolio"/>
+    <HelloWorld msg="new Tank portfoli"/>
   </div>
 </template>
 
